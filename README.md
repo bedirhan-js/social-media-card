6273c78c3eeda45905434fcb650ddd5537373e1d
bedirhan-js <bedirhanfx@gmail.com>
2023-08-08 00:54:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Kişisel web sitesi</h1>
+
+Bu proje sadece sosyal medya profilleri içere basit bir sayfadır.
+
+<h2>Kullanılan teklonojiler</h2>
+
+HTML ve CSS kullanılarak oluşturulmuştur
+
+<h2>Örnek ekran görüntüsü</h2>
+
+![](card.gif)
---
+++ bilgiler.js
@@ -0,0 +1,19 @@
+const twitter = document.getElementById("twitter");
+const instagram = document.getElementById("instagram");
+const tiktok = document.getElementById("tiktok");
+const facebook = document.getElementById("facebook");
+const youtube = document.getElementById("youtube");
+const paypal = document.getElementById("paypal");
+const whatsapp = document.getElementById("whatsapp");
+const eposta = document.getElementById("eposta");
+
+// Butonların linklerini değiştirmek için 2 tırnak arasına istediğiniz linki kopyalayın ↓↓↓↓↓↓↓
+
+twitter.href = "https://github.com/bedirhan-js?tab=repositories";
+instagram.href = "https://github.com/bedirhan-js?tab=repositories";
+tiktok.href = "https://github.com/bedirhan-js?tab=repositories";
+facebook.href = "https://github.com/bedirhan-js?tab=repositories";
+youtube.href = "https://github.com/bedirhan-js?tab=repositories";
+paypal.href = "https://github.com/bedirhan-js?tab=repositories";
+whatsapp.href = "https://github.com/bedirhan-js?tab=repositories";
+eposta.href = "https://github.com/bedirhan-js?tab=repositories";
